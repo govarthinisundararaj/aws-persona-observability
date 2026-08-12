dd349fe2dca118210e34d5b15999c3bf1f5c3f2d
govarthinisundararaj <35028923+govarthinisundararaj@users.noreply.github.com>
2026-08-12 16:37:35 +0530

    Create versions.tf

---
+++ terraform/versions.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+
+  backend "s3" {
+    bucket       = "aws-persona-observability-tfstate-605618832779"
+    key          = "terraform/state.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
